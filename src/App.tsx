df0727cea6be87e28420452942bcffe316511e61
oozaryanci <oozayranci@gmail.com>
2021-03-09 11:43:15 +0200

    blockchain.info/ticker async fuction is created with its values

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 import { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
+import { Wrapper } from './styles/App.styles';
+
+type BitcoinData = {
+  '15m': number;
+  buy: number;
+  last: number;
+  sell: number;
+  symbol: string;
+}
+
+type Currencies = {
+  [key: string]: BitcoinData;
+}
+
+const getBCData = async (): Promise<Currencies> => 
+  await (await fetch('https://blockchain.info/ticker')).json();
+
 
 const App = () => {
   return (
